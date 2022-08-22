1aad783b081c9d0c077eb929af523138a79c53e3
Anel Git <anelx373@hotmail.com>
2022-08-22 22:09:24 +0200

    "Added a bunch of stuff"

--- react-videoteka/src/App.js
+++ react-videoteka/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import MovieComponent from "./MovieComponent";
+import MovieInfoComponent from "./MovieInfoComponent";
+const API_KEY = "c54a7477";
+const logo = require("./movie-icon.jpg");
+const search = require("./search-icon.png");
+const axios = require("axios");
 
 function App() {
+  const [searchQuery, updateSearchQ] = useState();
+  const [timeoutId, updateTimeoutId] = useState();
+  const [movieList, updateMovieList] = useState([]);
+  const [selectMovie, onMovieSelect] = useState();
+
+  const fetchData = async (searchString) => {
+    const response = await axios.get(
+      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`
+    );
+    updateMovieList(response.data.Search);
+  };
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId);
+    updateSearchQ(event.target.value);
+    const timeout = setTimeout(() => fetchData(event.target.value), 500);
+    updateTimeoutId(timeout);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Container">
+      <header className="Header">
+        <div className="AppName">
+          <img src={logo} className="MovieImage" alt="" />
+          Videoteka
+        </div>
+        <div className="SearchBox">
+          <img src={search} className="SearchImage" alt="" />
+          <input
+            className="SearchInput"
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </div>
       </header>
+      {selectMovie && (
+        <MovieInfoComponent
+          selectMovie={selectMovie}
+          onMovieSelect={onMovieSelect}
+        />
+      )}
+      <div className="MovieListContainer">
+        {movieList?.length
+          ? movieList.map((movie, index) => (
+              <MovieComponent
+                key={index}
+                movie={movie}
+                onMovieSelect={onMovieSelect}
+              />
+            ))
+          : "No Movie search"}
+      </div>
     </div>
   );
 }
